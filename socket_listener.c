9ad296bb307ba03e7b9d651f0ff2f199ed54faea
csimeone23 <csimeone1223@gmail.com>
2022-04-22 13:52:27 -0400

    idk trying localhost

--- socket_listener.c
+++ socket_listener.c
@@ -131,9 +131,9 @@ int main(int argc, char *argv[]){
 	char* rpi_ip = "192.168.1.205";			// TODO: Get these via function call
 	char* broadcast_ip = "192.168.2.1";		// TODO: Get these via function call
 
-	create_udp_socket(&internet_to_rpi_bridge_socket, "192.168.1.205", 8080);	// This IP is the R-PI's
+	create_udp_socket(&internet_to_rpi_bridge_socket, "127.0.0.1", 8080);	// This IP is the R-PI's
 	create_udp_socket(&rpi_ethernet_BROADCAST_socket, "192.168.2.1", 3074);		// This is the broadcast address so that we can broadcast packets from internet to xbox
-	create_udp_socket(&rpi_ethernet_DIRECT_socket, "192.168.1.205", 3074);
+	create_udp_socket(&rpi_ethernet_DIRECT_socket, "127.0.0.1", 3074);
 
 	struct thread_data t_data[2];
 	t_data[0].socket = &internet_to_rpi_bridge_socket;
