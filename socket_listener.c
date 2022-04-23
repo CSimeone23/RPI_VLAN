b2a6ea648a16d726deeb3101992e1397069888b6
csimeone23 <csimeone1223@gmail.com>
2022-04-23 14:16:23 -0400

    Moved code around, nothing new

--- socket_listener.c
+++ socket_listener.c
@@ -164,6 +164,10 @@ int main(int argc, char *argv[]){
 	create_udp_socket(&rpi_ethernet_DIRECT_socket, "192.168.1.205", 3074);
 	printf("DEBUG AFTER: %d\n", internet_to_rpi_bridge_socket);
 
+	// Establish Communications with Hubserver
+	setSocketToCommunicateWithHubServer(internet_to_rpi_bridge_socket);
+
+	// Create threads for the sockets we just made
 	struct thread_data t_data[2];
 	t_data[0].socket = &internet_to_rpi_bridge_socket;
 	t_data[0].port_num = 8080;
@@ -174,11 +178,7 @@ int main(int argc, char *argv[]){
 	t_data[2].socket = &rpi_ethernet_DIRECT_socket;
 	t_data[2].port_num = 3074;
 	t_data[2].thread_id = 3;
-
-	// Establish Communications with Hubserver
-	setSocketToCommunicateWithHubServer(internet_to_rpi_bridge_socket);
-
-	// Create threads for the sockets we just made
+	
 	create_listener_thread_wifi(&internet_to_rpi_bridge_socket, &t_data[0]);
 	create_listener_thread_eth(&rpi_ethernet_BROADCAST_socket, &t_data[1]);
 	create_listener_thread_eth(&rpi_ethernet_DIRECT_socket, &t_data[2]);
