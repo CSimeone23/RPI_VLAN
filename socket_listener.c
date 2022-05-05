c33811e7ea9de14cbea93ee590253ae89f1c1f17
csimeone23 <csimeone1223@gmail.com>
2022-05-04 20:43:34 -0400

    Syntax error fix

--- socket_listener.c
+++ socket_listener.c
@@ -94,7 +94,7 @@ void *udp_listener_thread(void *arg){
 		}
 		printf("Thread #%d Received: \"%s\"\n\tFrom: %s:%d\n", t_data->thread_id, buf, inet_ntoa(incoming_connection_address.sin_addr), ntohs(incoming_connection_address.sin_port));
 		/* TEMP */
-		if(strcmp(inet_ntoa(incoming_connection_address), "0.0.0.1") == 0){
+		if(strcmp(inet_ntoa(incoming_connection_address.sin_addr), "0.0.0.1") == 0){
 			write_data_to_file(buf);
 		}
 		// Make sure we dont get stuck in a loop
