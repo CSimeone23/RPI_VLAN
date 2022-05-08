35901956cb98d33f5e36794728abca91b7c29047
csimeone23 <csimeone1223@gmail.com>
2022-05-08 16:31:16 -0400

    removed loop catcher

--- socket_listener.c
+++ socket_listener.c
@@ -22,6 +22,8 @@ struct sockaddr_in HUBSERVER_ADDRESS;
 // TEMP ////////////////////////////////////
 struct sockaddr_in TEMP_DIRECT_XBOX_ADDRESS;
 struct sockaddr_in PHAUX_ADDRESS;
+char *DATAGRAM = malloc(215*sizeof(char));
+int SAVED_DATAGRAM = 0;
 ////////////////////////////////////////////
 
 pthread_t threads[NUM_THREADS];
