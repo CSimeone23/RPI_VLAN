82bbdc0d5748d8d34b851947b86e228069390a9e
csimeone23 <csimeone1223@gmail.com>
2022-10-03 16:45:16 -0400

    changes param

--- src/DEBUG_HELPERS/packet_sniffer.c
+++ src/DEBUG_HELPERS/packet_sniffer.c
@@ -32,7 +32,7 @@ int main()
 	if(logfile==NULL) printf("Unable to create file.");
 	printf("Starting...\n");
 	//Create a raw socket that shall sniff
-	sock_raw = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	sock_raw = socket(AF_INET, SOCK_RAW, 0);
 	if(sock_raw < 0)
 	{
 		printf("Socket Error\n");
