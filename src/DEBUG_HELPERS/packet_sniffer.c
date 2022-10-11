a833e0cfd813639a1f552ae52e1d32c4a376a1b7
csimeone23 <csimeone1223@gmail.com>
2022-10-11 18:35:01 -0400

    changes

--- src/DEBUG_HELPERS/packet_sniffer.c
+++ src/DEBUG_HELPERS/packet_sniffer.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
   }
 
   printf("Starting...\n"); 
-  raw_socket = socket(AF_PACKET, SOCK_RAW, 0);
+  raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
   if(raw_socket < 0) {
     printf("Error Creating socket, error_code: %d\n", errno);
     exit(EXIT_FAILURE);
