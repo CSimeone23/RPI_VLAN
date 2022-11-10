1e799949629fe470fdee6c871de5591f8f3892fe
csimeone23 <csimeone1223@gmail.com>
2022-11-10 12:53:41 -0500

    Added counter for listening to packets

--- packet_sniffer.c
+++ packet_sniffer.c
@@ -12,6 +12,16 @@
 FILE *logfile;
 int raw_socket;
 
+struct sockaddr_ll {
+  unsigned short sll_family;   /* Always AF_PACKET */
+  unsigned short sll_protocol; /* Physical-layer protocol */
+  int            sll_ifindex;  /* Interface number */
+  unsigned short sll_hatype;   /* ARP hardware type */
+  unsigned char  sll_pkttype;  /* Packet type */
+  unsigned char  sll_halen;    /* Length of address */
+  unsigned char  sll_addr[8];  /* Physical-layer address */
+};
+
 int main(int argc, char *argv[]) {
   int saddr_size, data_size;
   struct sockaddr saddr;
@@ -32,10 +42,12 @@ int main(int argc, char *argv[]) {
     printf("Error Creating socket, error_code: %d\n", errno);
     exit(EXIT_FAILURE);
   }
-
   printf("Created Socket!\n");
+
   printf("Listening...\n");
-  while(1) {
+  int maxCount = 20;
+  int currentCount = 0;
+  while(currentCount < maxCount) {
     saddr_size = sizeof saddr;
     data_size = recvfrom(raw_socket, buffer, MAX_BUFFER_SIZE, 0, &saddr, &saddr_size);
     if(data_size < 0) {
@@ -43,6 +55,7 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     }
     printf("Buffer = %s", buffer);
+    currentCount++;
   } // End of while loop
 
   printf("Finished Execution :)\n");
