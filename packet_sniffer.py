3f13338dc90bd20f8011dc9db52e244a454a6864
csimeone23 <csimeone1223@gmail.com>
2023-07-02 12:37:22 -0400

    updated sockets

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -15,30 +15,35 @@ WIFI_PORT = 5000
 ETHERNET_IP = "192.168.2.1"
 ETHERNET_PORT = 9999
 
-def hubserver_to_wifi_listener(wifi_socket, ethernet_socket):
+def hubserver_to_wifi_listener(wifi_socket, broadcast_socket):
     # Hubserver -> Local
     # For now this will handle the 'Searching' ping
     # If this is hit then it should be sent to broadcast ip
     while True:
         data, addr = wifi_socket.recvfrom(MAX_BUFFER_SIZE)
         print(f"[{WIFI_IP}:{WIFI_PORT}] received message: {data}")
-        ethernet_socket.sendto(data, (BROADCAST_IP, UDP_PORT))
+        broadcast_socket.sendto(data, (BROADCAST_IP, UDP_PORT))
         print("Sent data from wifi -> ethernet -> broadcast")
 
-def broadcast_listener(broadcast_socket):
+def broadcast_listener(broadcast_socket, wifi_socket):
     # Whatever -> broadcast
     # Assuming that it's just the search ping we can
     # send this directly to the hubserver
     # THIS IS GOING TO CHANGE
     while True:
         data, addr = broadcast_socket.recvfrom(MAX_BUFFER_SIZE)
-        print("[255.255.255.255:3074] received message: %s" % data)
-        broadcast_socket.sendto(data, (HUBSERVER_IP, HUBSERVER_PORT))
+        print(f"[{BROADCAST_IP}:{UDP_PORT}] received message: %s" % data)
+        if addr[0] == '192.168.2.1':
+            print('Just a packet from WIFI... not interested')
+            continue
+        wifi_socket.sendto(data, (HUBSERVER_IP, HUBSERVER_PORT))
         print("Broadcast socket -> hubserver DONE")
 
 if __name__ == "__main__":
     udp_sock_3074 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_sock_3074.bind((BROADCAST_IP, UDP_PORT))
+    udp_sock_3074.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    udp_sock_3074.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str("eth0" + '\0').encode('utf-8'))
     
     wifi_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     wifi_sock.bind((WIFI_IP, WIFI_PORT))
@@ -48,9 +53,9 @@ if __name__ == "__main__":
     ethernet_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
     print("Creating Threads!")
-    t1 = threading.Thread(target=broadcast_listener, args=(udp_sock_3074, ))
-    t2 = threading.Thread(target=hubserver_to_wifi_listener, args=(wifi_sock, ethernet_sock,))
+    t1 = threading.Thread(target=broadcast_listener, args=(udp_sock_3074, wifi_sock, ))
+    t2 = threading.Thread(target=hubserver_to_wifi_listener, args=(wifi_sock, udp_sock_3074,))
     t1.start()
-    print('Thread 1 Started')
+    print('Thread 1 Started!')
     t2.start()
     print('Thread 2 started!')
